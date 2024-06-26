e7f82720816ff4d35ad4155e0d48edc63541c55f
Jarkko Tulensalo <jtulensalo@gmail.com>
2024-06-26 16:07:48 +0300

    removing unnecessary functions

--- src/vector_search/embeddings.py
+++ src/vector_search/embeddings.py
@@ -1,62 +1,12 @@
-from typing import Dict, List, Tuple
+from typing import Dict, List
 
 import numpy as np
 import PIL
 import torch
-from PIL import Image, ImageFile
+from PIL import ImageFile
 
 # Allow loading of truncated images
 ImageFile.LOAD_TRUNCATED_IMAGES = True
-
-
-def preprocess_image(image_path: str, image_size: int = 336) -> PIL.Image.Image:
-    """Preprocesses an image by resizing it to the desired size."""
-    try:
-        image = Image.open(image_path).convert("RGB")
-        resized_image = image.resize((image_size, image_size))
-
-        return resized_image
-    except (PIL.UnidentifiedImageError, OSError) as e:
-        print(f"Error loading image {image_path}: {e}")
-        return None  # or a default image or handling mechanism
-
-
-def preprocess_images_error_handling(
-    image_paths: List[str], metadata: List[Dict], image_size: int = 336
-) -> Tuple[List[PIL.Image.Image], List[str], List[Dict]]:
-    """
-    Preprocesses a list of images by resizing them to the desired size.
-    This function also handles errors that occur during image loading.
-    """
-    images = []
-    valid_image_paths = []
-    valid_metadatas = []
-    for i, image_path in enumerate(image_paths):
-        try:
-            image = Image.open(image_path).convert("RGB")
-            resized_image = image.resize((image_size, image_size))
-
-            images.append(resized_image)
-            valid_image_paths.append(image_path)
-            valid_metadatas.append(metadata[i])
-        except (PIL.UnidentifiedImageError, OSError):
-            # print(f"Error loading image {image_path}: {e}")
-            pass
-    return images, valid_image_paths, valid_metadatas
-
-
-def preprocess_images(
-    image_paths: List[str], metadata: List[Dict], image_size: int = 336
-) -> List[PIL.Image.Image]:
-    """
-    Preprocesses a batch of images by resizing them to the desired size.
-    """
-    images = []
-    for image_path in image_paths:
-        image = Image.open(image_path).convert("RGB")
-        resized_image = image.resize((image_size, image_size))
-        images.append(resized_image)
-    return images
 
 
 def extract_image_features(
@@ -69,7 +19,6 @@ def extract_image_features(
         images (List[PIL.Image.Image]): A list of PIL images.
         model (torch.nn.Module): A CLIP model.
         image_encoder (str): The name of the image encoder.
-        processor (torch.nn.Module): A CLIP processor.
 
     Returns:
         img_emb (np.ndarray): An array of image features.
@@ -81,11 +30,9 @@ def extract_image_features(
         or image_encoder == "clip-ViT-L-14"
         or image_encoder == "clip-vit-large-patch14-336"
     ):
-        with torch.no_grad():
-            img_emb = model.encode(images)
+        img_emb = model.encode(images)
     elif image_encoder == "jinaai/jina-clip-v1":
-        with torch.no_grad():
-            img_emb = model.encode_image(images)
+        img_emb = model.encode_image(images)
     return img_emb
 
 
