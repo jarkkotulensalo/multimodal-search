ce63d9d8443169a66dbe06a6a330e485ef6d72ab
Jarkko Tulensalo <jtulensalo@gmail.com>
2024-07-29 13:49:46 +0300

    minor changes

--- src/app/streamlit_utils.py
+++ src/app/streamlit_utils.py
@@ -12,7 +12,7 @@ def show_image_grid(results, top_k):
         top_k (int): The number of results to display.
     """
 
-    num_cols = 4  # Define number of columns in the grid
+    num_cols = 3  # Define number of columns in the grid
     rows = (top_k + num_cols - 1) // num_cols  # Calculate number of rows needed
 
     for row in range(rows):
@@ -22,12 +22,12 @@ def show_image_grid(results, top_k):
             if idx < len(results):
                 res = results[idx]
                 with cols[col_idx]:
-                    st.write(f"Result {idx + 1}")
+                    # st.write(f"Result {idx + 1}")
                     # st.write(f"Similarity: {res['similarity']:.2f}")
                     # st.write(f"Folder: {res['folder']}")
                     # st.write(f"Path: {res['path']}")
                     # st.write(f"{res['date_taken']}")
-                    st.write(f"{res['metadata']}")
+                    # st.write(f"{res['metadata']}")
                     if res["fpath"].lower().endswith((".mp4", ".mov", ".avi")):
                         st.video(res["fpath"])
                     else:
